66a5de1bd639eae049f45b5684963a61f4f5d769
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-08-18 14:35:24 +0700

    update

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+function showCompleteText() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("read-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
